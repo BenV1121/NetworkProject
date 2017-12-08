088c5d64476bd37861a3b8d41be54405c6297fa3
Benjamin Vongtawan <benajamin.vongtawan@students.aie.edu.au>
2017-12-08 11:08:02 -0800

    Controller and Cursor

---
+++ Assets/Scripts/CursorPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CursorPoint : NetworkBehaviour
+{
+    public Texture2D cursorTexture;
+    public Texture2D cursorClick;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Cursor.SetCursor(cursorClick, hotSpot, cursorMode);
+        }
+        else
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,24 +5,49 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-
     float force;
 
+    private Rigidbody2D rgb2;
+
+    public bool groundCheck = true;
     bool facingRight = true;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-		
+        rgb2 = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         float move = Input.GetAxis("Horizontal");
-
         force = move * speed;
-
         transform.Translate(new Vector2(force, 0));
+
+        if (Input.GetKeyDown("space") && groundCheck)
+        {
+            rgb2.AddForce(Vector2.up * 300);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+
+        }
 	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //if()
+        groundCheck = true;
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        groundCheck = true;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        groundCheck = false;
+    }
 }

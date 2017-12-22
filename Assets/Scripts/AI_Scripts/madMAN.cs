62f6255ececac3ab508c8bc81b61638aa5629d12
henry-banks <henry.banks@students.aie.edu.au>
2017-12-22 15:45:50 -0800

    Level is done-ish?

--- Assets/Scripts/AI_Scripts/madMAN.cs
+++ Assets/Scripts/AI_Scripts/madMAN.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class madMAN : MonoBehaviour {
 
+    bool isAwake;
+
     public float speed;
     public float stop;
    Transform target;
 	// Use this for initialization
 	void Start () {
-        target = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (target == null)
+            target = PlayerManager.players[(int)Random.Range(0, PlayerManager.players.Count)].gameObject.transform;
+
         if (Vector2.Distance(transform.position, target.position) > stop)
         { 
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);

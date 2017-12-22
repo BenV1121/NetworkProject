9a9bb016c0545c3fe84e023842fdd0656bb87a00
Benjamin Vongtawan <benajamin.vongtawan@students.aie.edu.au>
2017-12-22 11:45:08 -0800

    Boss health updates

---
+++ Assets/Scripts/WarningScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningScript : MonoBehaviour
+{
+    public bool animationEnded = false;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(animationEnded)
+        {
+            Destroy(gameObject);
+        }
+	}
+}

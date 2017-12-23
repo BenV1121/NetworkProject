24289b31a19e123947325a70f6ce0c130a6b1322
henry-banks <henry.banks@students.aie.edu.au>
2017-12-22 16:15:30 -0800

    I HOPE THIS DOES NOT BREAK IT ALL

--- Assets/Scripts/AI_Scripts/madMAN.cs
+++ Assets/Scripts/AI_Scripts/madMAN.cs
@@ -4,23 +4,61 @@ using UnityEngine;
 
 public class madMAN : MonoBehaviour {
 
-    bool isAwake;
-
     public float speed;
     public float stop;
+    public string PlayerTag = "Player";
+    public float triggerRang = 15;
+
    Transform target;
-	// Use this for initialization
-	void Start () {
-	}
+    // Use this for initializatio
+
+ 
+
+    void Start () {
+        target = GameObject.FindGameObjectWithTag(PlayerTag).GetComponent<Transform>();
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (target == null)
-            target = PlayerManager.players[(int)Random.Range(0, PlayerManager.players.Count)].gameObject.transform;
 
-        if (Vector2.Distance(transform.position, target.position) > stop)
-        { 
+
+        if (target != null && Vector2.Distance(transform.position, target.position) > stop)
+        {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
         }
-	}
+
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag(PlayerTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestPlayer = null;
+        foreach (GameObject player in players)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, player.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestPlayer = player;
+            }
+        }
+
+        if (nearestPlayer != null && shortestDistance <= triggerRang)
+        {
+            target = nearestPlayer.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, triggerRang);
+    }
 }
